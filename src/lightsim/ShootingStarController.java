647fc0e4393e362811f5d9e64a78e98659be6d83
George Huo <george@paygarden.com>
2016-08-29 04:19:26 -0700

    wip - clean up comments

--- src/lightsim/ShootingStarController.java
+++ src/lightsim/ShootingStarController.java
@@ -3,7 +3,6 @@ package lightsim;
 import java.awt.Color;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Random;
 import lightsim.LightArray.Light;
 
@@ -95,13 +94,9 @@ public class ShootingStarController extends LightController {
         int trailLength = 2;
         double stretch;
         int fadeDuration;
-        
-        HashMap stretches;
-        HashMap fadeDurations;            
-    
+            
         public StarAnimation(int columnIndex) {
             this.columnIndex = columnIndex;
-            this.stretches = new HashMap();
             Random r = new Random();
             this.stretch = 0.3 + (r.nextDouble() * 0.5);
             this.fadeDuration = 8 + (r.nextInt(5));
@@ -109,27 +104,7 @@ public class ShootingStarController extends LightController {
         
         private double getLightIntensity(int pos, double t) {
             t = t * STAR_DURATION;
-            
-            Random r = new Random();
-
-            /*
-            Double dStretch = (Double) stretches.get(pos);
-            if (dStretch == null) {
-              double newStretch = 0.3 + (r.nextDouble() * 0.5);
-              dStretch = new Double(newStretch);
-              stretches.put(pos, dStretch);            
-            }
-            double stretch = dStretch.doubleValue();
-            
-            Integer iFadeDuration = (Integer) fadeDurations.get(pos);
-            if (iFadeDuration == null) {
-              int newFadeDuration = 8 + (r.nextInt(5));
-              iFadeDuration = new Integer(newFadeDuration);
-              fadeDurations.put(pos, iFadeDuration);
-            }
-            int fadeDuration = iFadeDuration.intValue();
-            */
-            
+                        
             double heightFactor = 0.05 + (pos - 1) * 0.1;
             double maxIntensity = 0.1 + (heightFactor * 0.9);
             
@@ -137,100 +112,44 @@ public class ShootingStarController extends LightController {
             double peakTime =  ((pos + 1) * stretch);
             double fadedTime = ((pos + 1 + fadeDuration) * stretch);
             
-            // fade in:  0 -> 1s
-            // fade out: 1 -> 3s
+            // e.g. fade in:  0 -> 1s
+            // e.g. fade out: 1 -> 3s
             double intensity = 0.0;
             String state = "";
             if ((t < startTime) || (t > fadedTime)) {
                 intensity = 0.0;
             } else if (startTime <= t && t < peakTime) {
-                // Console.log("  " + pos + " pre-peak");
                 state = "pre-peak";
                 intensity = maxIntensity - ((peakTime - t) / stretch * maxIntensity);
             } else if (peakTime <= t && t < fadedTime) {
-                // intensity = 0;
-                // Console.log("  " + pos + " fading");
                 state = "fading";
                 intensity = ((fadedTime - t) / (fadeDuration * stretch) * maxIntensity);
             }
             
             if (intensity > 0 && pos == 5) {
-            Console.log(
-                "time " + formatLogDouble(t) +
-                " pos " + pos +
-                " maxIntensity " + formatLogDouble(maxIntensity) +
-                " s " + formatLogDouble(startTime) +
-                " e " + formatLogDouble(fadedTime) +
-                " intensity " + formatLogDouble(intensity) +
-                    " " + state
-            );
+                Console.log(
+                    "time " + formatLogDouble(t) +
+                    " pos " + pos +
+                    " maxIntensity " + formatLogDouble(maxIntensity) +
+                    " s " + formatLogDouble(startTime) +
+                    " e " + formatLogDouble(fadedTime) +
+                    " intensity " + formatLogDouble(intensity) +
+                        " " + state
+                );
             }
             return intensity;
         }
         
         public void update(double t) {
-            double starHeight = 10 * t - 0.5;  // starHeight ranges from -0.5 to 9.5
-            double intensity = 0.1 + t * 0.9;  // intensity ranges from 0.1 to 1.
-            // double intensity = 1.0;
- 
-            /*
-            double upperLight = Math.ceil(starHeight);
-            double[] positions = new double[trailLength];
-            double[] intensities = new double[trailLength];
-            for (int i = 0; i < trailLength; ++i) {
-              positions[i] = upperLight - trailLength + i;
-              intensities[i] = Math.min(1.0, intensity * (1 - (positions[i] - starHeight)));
-            }
-            */
-            double lowerLight = Math.floor(starHeight);
-            double upperLight = Math.ceil(starHeight);
-            
+            // double starHeight = 10 * t - 0.5;  // starHeight ranges from -0.5 to 9.5
+            // double intensity = 0.1 + t * 0.9;  // intensity ranges from 0.1 to 1.
+            // double lowerLight = Math.floor(starHeight);
+            // double upperLight = Math.ceil(starHeight);
             // Interpolate intensity. Conveniently, lights are 1 unit apart so
             // no need to normalize.
-            double lowerIntensity = intensity * (1 - (starHeight - lowerLight));
-            double upperIntensity = intensity * (1 - (upperLight - starHeight));
+            // double lowerIntensity = intensity * (1 - (starHeight - lowerLight));
+            // double upperIntensity = intensity * (1 - (upperLight - starHeight));
 
-            /*
-            Console.log(
-                "time " + formatLogDouble(t) +
-                // " lh " + Math.floor(starHeight) +
-                // " uh " + Math.ceil(starHeight) +
-                " lh " +  (int) positions[0] +
-                " uh " + (int) positions[trailLength - 1] +
-                " li " + formatLogDouble(intensities[0]) +
-                " ui " + formatLogDouble(intensities[trailLength - 1])
-            );
-*/
-            /*
-            Console.log(
-                "time " + formatLogDouble(t) +
-                " starHeight " + formatLogDouble(starHeight) +
-                " intensity " + formatLogDouble(intensity) +
-                " lh " + Math.floor(starHeight) +
-                " uh " + Math.ceil(starHeight) +
-                //" lh " +  (int) positions[0] +
-                //" uh " + (int) positions[trailLength - 1] +
-                " li " + formatLogDouble(lowerIntensity) +
-                " ui " + formatLogDouble(upperIntensity)
-            );
-            */
-
- 
-            /*
-            for (int i = 0; i < trailLength; ++i) {
-                if (positions[i] >= 0 && positions[i] < 10) {
-                   addIntensityToLight(strings[columnIndex][9 - (int)positions[i]], intensities[i]);                
-                }
-            }
-*/
-            
-            // Now write to the lights.
-            if (lowerLight >= 0 && lowerLight < 10) {
-                //addIntensityToLight(strings[columnIndex][9 - (int)lowerLight], lowerIntensity);
-            }
-            if (upperLight >=0 && upperLight < 10) {
-                //addIntensityToLight(strings[columnIndex][9 - (int)upperLight], upperIntensity);
-            }
             for (int i = 0; i < 10; ++i) {
                 addIntensityToLight(strings[columnIndex][9 - i], getLightIntensity(i, t));
             }
@@ -254,7 +173,7 @@ public class ShootingStarController extends LightController {
         private void addIntensityToLight(Light light, double intensity) {
             Color currentColor = light.getColor();
             Color newColor = interpolateColors(currentColor, starColor, intensity);
-            //Console.log("interpolate: orig " + currentColor.toString() + " target " + starColor.toString() + " interp " + newColor.toString());
+            // Console.log("interpolate: orig " + currentColor.toString() + " target " + starColor.toString() + " interp " + newColor.toString());
             light.setColor(newColor);
         }
     }
