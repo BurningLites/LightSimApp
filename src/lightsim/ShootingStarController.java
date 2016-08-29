ac7e09a183f403b7b09152b92dcd277a25a24915
George Huo <george@paygarden.com>
2016-08-29 05:42:11 -0700

    wip - working multiple star strands

--- src/lightsim/ShootingStarController.java
+++ src/lightsim/ShootingStarController.java
@@ -3,6 +3,7 @@ package lightsim;
 import java.awt.Color;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 import lightsim.LightArray.Light;
 
@@ -10,7 +11,6 @@ public class ShootingStarController extends LightController {
     // Average frequency of star generation, in stars/second.
     static final double STAR_FREQUENCY = 5;
     static final double STAR_DURATION = 20;
-    static final double INTENSITY_MULTIPLIER = 200;
     
     static Color bgColor = new Color(0, 0, 100);
     static Color starColor = new Color(255, 255, 0);
@@ -21,6 +21,7 @@ public class ShootingStarController extends LightController {
     LightArray.Light strings[][];
     LightArray lightArray;
     ArrayList<StarAnimation> animations;
+    HashMap activeStrings;
     Random random;
     
     double lastStepTime = 0;
@@ -30,11 +31,8 @@ public class ShootingStarController extends LightController {
         strings = light_array.getStrings();
         random = new Random();
         animations = new ArrayList<>();
-        StarAnimation animation = new StarAnimation(0);
-        animation.setDuration(STAR_DURATION);
-        animation.start(0);
-        animations.add(animation);
-
+        activeStrings = new HashMap();
+        createAnimation(0, 0); 
     }
     
     @Override
@@ -50,32 +48,49 @@ public class ShootingStarController extends LightController {
         // Randomly add some star animations.
         double dt = timeSeconds - lastStepTime;
         lastStepTime = timeSeconds;
-        //addNewStars(dt, timeSeconds);
+        addNewStars(dt, timeSeconds);
         updateAnimations(timeSeconds);
         return true;
     }
     
+    private void createAnimation(int string, double startTime) {
+        Console.log("starting string " + string);
+        StarAnimation animation = new StarAnimation(string);
+        animation.setDuration(STAR_DURATION);
+        animation.start(startTime);
+        animations.add(animation);
+        activeStrings.put(string, Boolean.TRUE);
+
+    }
+    
     private void addNewStars(double dt, double time) {
+        int numActive = animations.size();
         double targetStarsToAdd = dt * STAR_FREQUENCY;
         
         // Calculate how many stars to add. For fractional stars, roll a random
         // number to see whether to include a star.
         int starsToAdd = (int)Math.floor(targetStarsToAdd) +
             (random.nextDouble() < (targetStarsToAdd % 1) ? 1 : 0);
+        // Console.log("target to add: " + starsToAdd);
         for (int i = 0; i < starsToAdd; i++) {
             int stringNum = random.nextInt(50);
-            StarAnimation animation = new StarAnimation(stringNum);
-            animation.setDuration(STAR_DURATION);
-            animation.start(time);
-            animations.add(animation);
+            
+            Boolean isActive = (Boolean) activeStrings.getOrDefault(stringNum, Boolean.FALSE);
+            Console.log("string " + stringNum + " isActive " + isActive.booleanValue());
+            if (!(isActive.equals(Boolean.TRUE))) {
+                createAnimation(stringNum, time);
+            }
         }
     }
     
     private void updateAnimations(double time) {
-        ArrayList<Animation> animationsToRemove = new ArrayList<>();
-        for (Animation animation : animations) {
-            if (animation.tick(time) == false) {
+        ArrayList<StarAnimation> animationsToRemove = new ArrayList<>();
+        for (StarAnimation animation : animations) {
+            animation.tick(time);
+            if (animation.isFinished(time)) {
+                Console.log("removing string " + animation.getColumnIndex());
                 animationsToRemove.add(animation);
+                activeStrings.put(animation.getColumnIndex(), Boolean.FALSE);
             }
         }
         animations.removeAll(animationsToRemove);
@@ -90,28 +105,38 @@ public class ShootingStarController extends LightController {
     
     public class StarAnimation extends Animation {
     
-        int columnIndex = 0;       
-        int trailLength = 2;
+        double intensityBase = 0.3;
+        int columnIndex = 0;
         double stretch;
         int fadeDuration;
-            
+        int posOffset;
+        
         public StarAnimation(int columnIndex) {
-            this.columnIndex = columnIndex;
             Random r = new Random();
-            this.stretch = 0.3 + (r.nextDouble() * 0.5);
-            this.fadeDuration = 8 + (r.nextInt(5));
+            this.columnIndex = columnIndex;
+            this.stretch = 0.2 + (r.nextDouble() * 0.4);
+            this.fadeDuration = 4 + (r.nextInt(4));
+            // this.posOffset = 0 - (r.nextInt(5));
+            this.posOffset = 0;
+            Console.log("added string " + columnIndex + 
+                " stretch " + stretch +
+                " fadeDuration " + fadeDuration +
+                " offset " + posOffset);
+        }
+        public int getColumnIndex() {
+            return this.columnIndex;
         }
         
         private double getLightIntensity(int pos, double t) {
             t = t * STAR_DURATION;
-                        
+            // Console.log("  getLightIntensity pos " + pos + " t " + formatLogDouble(t));
             double heightFactor = 0.05 + (pos - 1) * 0.1;
-            double maxIntensity = 0.1 + (heightFactor * 0.9);
+            double maxIntensity = intensityBase + (heightFactor * (1.0 - intensityBase));
             
             double startTime = (pos * stretch);
             double peakTime =  ((pos + 1) * stretch);
             double fadedTime = ((pos + 1 + fadeDuration) * stretch);
-            
+                     
             // e.g. fade in:  0 -> 1s
             // e.g. fade out: 1 -> 3s
             double intensity = 0.0;
@@ -125,10 +150,11 @@ public class ShootingStarController extends LightController {
                 state = "fading";
                 intensity = ((fadedTime - t) / (fadeDuration * stretch) * maxIntensity);
             }
+            intensity = Math.min(intensity, 1.0);
             
-            if (intensity > 0 && pos == 5) {
+            if (intensity > 0 && false) {
                 Console.log(
-                    "time " + formatLogDouble(t) +
+                    "string " + this.columnIndex + " time " + formatLogDouble(t) +
                     " pos " + pos +
                     " maxIntensity " + formatLogDouble(maxIntensity) +
                     " s " + formatLogDouble(startTime) +
@@ -138,6 +164,10 @@ public class ShootingStarController extends LightController {
                 );
             }
             return intensity;
+        }
+        
+        public boolean isFinished(double t) {
+          return (t - this.startTimeSeconds) > (10 + fadeDuration) * stretch;
         }
         
         public void update(double t) {
@@ -151,7 +181,9 @@ public class ShootingStarController extends LightController {
             // double upperIntensity = intensity * (1 - (upperLight - starHeight));
 
             for (int i = 0; i < 10; ++i) {
-                addIntensityToLight(strings[columnIndex][9 - i], getLightIntensity(i, t));
+                if (9 - i + posOffset >= 0) {
+                  addIntensityToLight(strings[columnIndex][9 - i + posOffset], getLightIntensity(i, t));
+                }
             }
         }
         
@@ -163,6 +195,10 @@ public class ShootingStarController extends LightController {
             return (int) Math.round(x * (1.0 - p) + y * p);
         }
         private Color interpolateColors(Color x, Color y, double p) {
+            //Console.log ("interpolate " + x + " " + y + " " + formatLogDouble(p) + 
+            //    interpolateInts(x.getRed(), y.getRed(), p) + " " +
+            //    interpolateInts(x.getGreen(), y.getGreen(), p) + " " +
+            //    interpolateInts(x.getBlue(), y.getBlue(), p));
             Color c = new Color(
                 interpolateInts(x.getRed(), y.getRed(), p),
                 interpolateInts(x.getGreen(), y.getGreen(), p),
@@ -173,7 +209,12 @@ public class ShootingStarController extends LightController {
         private void addIntensityToLight(Light light, double intensity) {
             Color currentColor = light.getColor();
             Color newColor = interpolateColors(currentColor, starColor, intensity);
-            // Console.log("interpolate: orig " + currentColor.toString() + " target " + starColor.toString() + " interp " + newColor.toString());
+            /*
+            Console.log("interpolate: orig " + currentColor.toString() +
+                " target " + starColor.toString() +
+                " interp " + newColor.toString() +
+                " p " + intensity);
+*/
             light.setColor(newColor);
         }
     }
